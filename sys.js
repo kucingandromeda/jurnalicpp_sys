d6b60abbd080adb9aff441ab0681743b19899c65
kucingandromeda <agvayazem@gmail.com>
2024-02-28 13:19:14 +0700

    sudah membuat sebuah fungsi yang dimana resultnya akan mengarah ke dirktory result di luarnya

--- sys.js
+++ sys.js
@@ -1,28 +1,39 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("miranda.txt", { encoding: "utf8" });
+const sys = (txt, cb) => {
+  const data = fs.readFileSync(`${txt}.txt`, { encoding: "utf8" });
 
-const data_split = data.split(/\n/);
+  const data_split = data.split(/\n/);
 
-const obj_result = {
-  judul: null,
-  penulis: [null, null, null, null],
-  genre: null,
-  url: null,
-  img: null,
+  const obj_result = {
+    judul: null,
+    penulis: [null, null, null, null],
+    genre: null,
+    url: null,
+    img: null,
+  };
+
+  const obj_list = [];
+  data_split.forEach((data) => {
+    const obj = {
+      type: "paragraf",
+      value: data,
+    };
+    obj_list.push(obj);
+  });
+
+  obj_result.isi = obj_list;
+
+  // console.log(obj_result);
+
+  fs.writeFileSync(
+    `./../result/${txt}.json`,
+    JSON.stringify(obj_result, null, 2)
+  );
+
+  return cb("finish bro");
 };
 
-const obj_list = [];
-data_split.forEach((data) => {
-  const obj = {
-    type: "paragraf",
-    value: data,
-  };
-  obj_list.push(obj);
+sys("miranda", (cb) => {
+  console.log(cb);
 });
-
-obj_result.isi = obj_list;
-
-// console.log(obj_result);
-
-fs.writeFileSync("./miranda.json", JSON.stringify(obj_result, null, 2));
